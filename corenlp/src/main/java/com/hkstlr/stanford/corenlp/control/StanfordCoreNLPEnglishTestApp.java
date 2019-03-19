177fe19d447f2dbe8b7e1729e07cf024ebae1737
hkastler <hkastler+github@gmail.com>
2019-03-18 19:06:45 -0500

    testapp with properties file

---
+++ corenlp/src/main/java/com/hkstlr/stanford/corenlp/control/StanfordCoreNLPEnglishTestApp.java
@@ -0,0 +1,18 @@
+package com.hkstlr.stanford.corenlp.control;
+
+import java.io.File;
+import java.io.IOException;
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+/** app for testing if Maven distribution is working properly */
+
+public class StanfordCoreNLPEnglishTestApp {
+    
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        File inputFile = new File(StanfordCoreNLPEnglishTestApp.class.getClassLoader().getResource("sample-english.txt").getFile());
+        File propsFile = new File(StanfordCoreNLPEnglishTestApp.class.getClassLoader().getResource("english.properties").getFile());
+        String[] englishArgs = new String[] { "-file", inputFile.getAbsolutePath(), "-props" , propsFile.getAbsolutePath() };
+        StanfordCoreNLP.main(englishArgs);
+    }
+}
