ad7653e8365d224a22acc8bdcee90c304552777e
hkastler <hkastler+github@gmail.com>
2019-03-19 11:32:28 -0500

    class for quick use SentimentPipeline

---
+++ corenlp/src/main/java/com/hkstlr/stanford/corenlp/control/StanfordCoreNLPSentimentPipelineMain.java
@@ -0,0 +1,19 @@
+package com.hkstlr.stanford.corenlp.control;
+
+import java.io.File;
+import java.io.IOException;
+
+import edu.stanford.nlp.sentiment.SentimentPipeline;
+
+/** app for testing if Maven distribution is working properly */
+
+public class StanfordCoreNLPSentimentPipelineMain {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        File inputFile = new File(StanfordCoreNLPSentimentPipelineMain.class.getClassLoader()
+                .getResource("sample-english.txt").getFile());
+        
+        args = new String[] { "-file", inputFile.getAbsolutePath() ,"-output", "root"};
+        
+        SentimentPipeline.main(args);
+    }
+}
